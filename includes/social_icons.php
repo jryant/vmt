cc6fc4d51f83e98cdf8332b6a4be613f32ce8725
Jason <hello@jasonryant.com>
2015-01-02 10:20:30 -0800

    Update

--- includes/social_icons.php
+++ includes/social_icons.php
@@ -3,18 +3,23 @@
 
 <?php if ( 'on' === et_get_option( 'divi_show_facebook_icon', 'on' ) ) : ?>
 	<li class="et-social-icon et-social-facebook">
-		<a href="<?php echo esc_url( et_get_option( 'divi_facebook_url', '#' ) ); ?>" class="icon">
+		<a href="<?php echo esc_url( et_get_option( 'divi_facebook_url', '#' ) ); ?>" class="icon" target="_blank">
 			<span><?php esc_html_e( 'Facebook', 'Divi' ); ?></span>
 		</a>
 	</li>
 <?php endif; ?>
 <?php if ( 'on' === et_get_option( 'divi_show_twitter_icon', 'on' ) ) : ?>
 	<li class="et-social-icon et-social-twitter">
-		<a href="<?php echo esc_url( et_get_option( 'divi_twitter_url', '#' ) ); ?>" class="icon">
+		<a href="<?php echo esc_url( et_get_option( 'divi_twitter_url', '#' ) ); ?>" class="icon" target="_blank">
 			<span><?php esc_html_e( 'Twitter', 'Divi' ); ?></span>
 		</a>
 	</li>
 <?php endif; ?>
+	<li class="et-social-icon et-social-linkedin">
+		<a href="http://ca.linkedin.com/in/jasonryant" class="icon" target="_blank">
+			<span><?php esc_html_e( 'LinkedIn', 'Divi' ); ?></span>
+		</a>
+	</li>
 <?php if ( 'on' === et_get_option( 'divi_show_google_icon', 'on' ) ) : ?>
 	<li class="et-social-icon et-social-google-plus">
 		<a href="<?php echo esc_url( et_get_option( 'divi_google_url', '#' ) ); ?>" class="icon">
